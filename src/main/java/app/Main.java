5b24b6cf8d0970c30081e34c80f9d66c60323394
ShawonAshraf <shawon13@live.com>
2019-01-26 18:57:00 +0600

    Main Driver

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,11 @@
 package app;
 
+import config.ScrapperConfiguration;
+import scrapper.Scrapper;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("INIT");
+        Scrapper cineplexScrapper = new Scrapper(ScrapperConfiguration.cineplexURLString);
+        cineplexScrapper.scrap();
     }
 }
