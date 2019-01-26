98602a3eb18b18c4d1edb54fe77f92471400a2ad
ShawonAshraf <shawon13@live.com>
2019-01-26 18:35:49 +0600

    config for scrapper

---
+++ src/main/java/config/ScrapperConfiguration.java
@@ -0,0 +1,11 @@
+package config;
+
+public class ScrapperConfiguration {
+    static {
+        String cineplexURLString = "";
+        String blockbusterURLString = "";
+
+        String cineplexJSONDumpFile = "/scrapper-dump/cineplex.json";
+        String blockbusterJSONDumpFile = "/scrapper-dump/blockbuster.json";
+    }
+}
