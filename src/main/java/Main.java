94164ca794c7095f3a2b673eef8a10294c53a38a
Shawon Ashraf <shawon13@live.com>
2019-01-28 02:11:20 +0600

    relocated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
-package app;
-
 import config.ScrapperConfiguration;
 import scrapper.Scrapper;
 
-@SuppressWarnings("WeakerAccess")
 public class Main {
     public static void main(String[] args) {
         Scrapper cineplexScrapper = new Scrapper(ScrapperConfiguration.cineplexURLString);
