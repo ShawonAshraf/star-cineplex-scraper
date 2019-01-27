f1f8fd4dcc0e0bc5c01772d4635bc4f7ad590b66
Shawon Ashraf <shawon13@live.com>
2019-01-27 23:53:09 +0600

    Webdriver init

--- src/main/java/scrapper/Scrapper.java
+++ src/main/java/scrapper/Scrapper.java
@@ -1,5 +1,12 @@
 package scrapper;
 
+import config.ScrapperConfiguration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
 public class Scrapper {
     private String urlToScrapFrom;
 
@@ -9,5 +16,19 @@ public class Scrapper {
 
     public void scrap() {
         System.out.println(String.format("Scrapping from => %s", urlToScrapFrom));
+
+        // init the firefox driver
+        try {
+            URL firefoxDriverURL = getClass().getClassLoader().getResource(ScrapperConfiguration.firefoxDriverURLString);
+            System.out.println("driver => " + firefoxDriverURL);
+            System.setProperty("webdriver.gecko.driver", Paths.get(firefoxDriverURL.toURI()).toFile().getAbsolutePath());
+
+            WebDriver webDriver = new FirefoxDriver();
+            
+            // get the url
+            webDriver.get(ScrapperConfiguration.cineplexURLString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
