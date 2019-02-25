08afcd4d868eab410fca232726b6a934b6478293
Shawon Ashraf <shawon13@live.com>
2019-02-25 23:36:10 +0600

    model class for Movie

---
+++ src/main/java/model/Movie.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Movie {
+    private String name;
+    private ArrayList<String> showTimes;
+
+    public Movie(String name, ArrayList<String> showTimes) {
+        this.name = name;
+        this.showTimes = showTimes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getShowTimes() {
+        return showTimes;
+    }
+}
