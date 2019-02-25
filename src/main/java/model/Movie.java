e18aee325208ea7f3e4b1cccfa406aa673a60db6
Shawon Ashraf <shawon13@live.com>
2019-02-26 01:02:07 +0600

    Movie Data class

--- src/main/java/model/Movie.java
+++ src/main/java/model/Movie.java
@@ -1,14 +1,19 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Movie {
     private String name;
     private ArrayList<String> showTimes;
+    private ArrayList<String> locations;
+    private ArrayList<Date> dates;
 
-    public Movie(String name, ArrayList<String> showTimes) {
+    public Movie(String name, ArrayList<String> showTimes, ArrayList<String> locations, ArrayList<Date> dates) {
         this.name = name;
         this.showTimes = showTimes;
+        this.locations = locations;
+        this.dates = dates;
     }
 
     public String getName() {
@@ -17,5 +22,23 @@ public class Movie {
 
     public ArrayList<String> getShowTimes() {
         return showTimes;
+    }
+
+    public ArrayList<String> getLocation() {
+        return locations;
+    }
+
+    public ArrayList<Date> getDates() {
+        return dates;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "name='" + name + '\'' +
+                ", showTimes=" + showTimes +
+                ", location='" + locations + '\'' +
+                ", dates=" + dates +
+                '}';
     }
 }
