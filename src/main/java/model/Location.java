454dcb67df1c7e34c0516c88afcb1cefe91bdb81
ShawonAshraf <shawon13@live.com>
2019-03-31 01:48:56 +0600

    location class

--- src/main/java/model/Location.java
+++ src/main/java/model/Location.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Location {
+    private String name;
+    private ArrayList<Movie> movies;
+
+    public Location(String name, ArrayList<Movie> movies) {
+        this.name = name;
+        this.movies = movies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
 }
