5eb919b44b045f78953db413cec369dcd6b15c2e
Shawon Ashraf <shawon13@live.com>
2019-04-13 11:14:56 +0600

    main updated with controllers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import controller.DbController;
 import controller.ShowKokhonController;
 
 public class Main {
     public static void main(String[] args) {
         ShowKokhonController controller = new ShowKokhonController();
         controller.run();
+
+        DbController db = new DbController();
+        db.write();
     }
 }
